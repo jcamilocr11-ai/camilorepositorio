f7680b0b6bc197f1fc56bfb50a2111743dbeea8c
jcamilocr11-ai <jcamilo.cr11@gmail.com>
2026-09-14 12:03:27 -0500

    responder

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 
 const servidor = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Servidor activo');
+
+    if (req.url === '/hola') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hola, servidor funcionando');
+    }
+
+    else if (req.url === '/saludo') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hola Camilo, bienvenido');
+    }
+
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Ruta no encontrada');
+    }
 });
 
 servidor.listen(3000, '0.0.0.0', () => {
-    console.log('Servidor ejecutandose en http://localhost:3000');
+    console.log('Servidor ejecutandose en el puerto 3000');
 });
