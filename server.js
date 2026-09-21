257c42435ee59a7e342f5ba17e9ed87d9d541ee9
Juan Camilo Cuaspud Rosero <jcamilo.cr11@gmail.com>
2026-09-21 09:46:49 -0500

    creaccion de csd

--- server.js
+++ server.js
@@ -1,23 +1,102 @@
 const http = require('http');
 
+let datos = [
+    { id: 1, nombre: 'Camilo' },
+    { id: 2, nombre: 'Juan' }
+];
+
 const servidor = http.createServer((req, res) => {
 
-    if (req.url === '/hola') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hola, servidor funcionando');
+    res.setHeader('Content-Type', 'application/json');
+
+    // GET - Mostrar
+    if (req.method === 'GET' && req.url === '/datos') {
+        res.writeHead(200);
+        res.end(JSON.stringify(datos));
     }
 
-    else if (req.url === '/saludo') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hola Camilo, bienvenido');
+    // POST - Agregar
+    else if (req.method === 'POST' && req.url === '/datos') {
+
+        let cuerpo = '';
+
+        req.on('data', parte => {
+            cuerpo += parte;
+        });
+
+        req.on('end', () => {
+            const nuevoDato = JSON.parse(cuerpo);
+
+            nuevoDato.id = datos.length + 1;
+            datos.push(nuevoDato);
+
+            res.writeHead(201);
+            res.end(JSON.stringify(nuevoDato));
+        });
     }
 
+    // PUT - Actualizar
+    else if (req.method === 'PUT' && req.url.startsWith('/datos/')) {
+
+        const id = parseInt(req.url.split('/')[2]);
+
+        let cuerpo = '';
+
+        req.on('data', parte => {
+            cuerpo += parte;
+        });
+
+        req.on('end', () => {
+
+            const dato = datos.find(d => d.id === id);
+
+            if (!dato) {
+                res.writeHead(404);
+                res.end(JSON.stringify({
+                    mensaje: 'Dato no encontrado'
+                }));
+                return;
+            }
+
+            const datosActualizados = JSON.parse(cuerpo);
+
+            dato.nombre = datosActualizados.nombre;
+
+            res.writeHead(200);
+            res.end(JSON.stringify(dato));
+        });
+    }
+
+    // DELETE - Eliminar
+    else if (req.method === 'DELETE' && req.url.startsWith('/datos/')) {
+
+        const id = parseInt(req.url.split('/')[2]);
+
+        const posicion = datos.findIndex(d => d.id === id);
+
+        if (posicion === -1) {
+            res.writeHead(404);
+            res.end(JSON.stringify({
+                mensaje: 'Dato no encontrado'
+            }));
+            return;
+        }
+
+        const eliminado = datos.splice(posicion, 1);
+
+        res.writeHead(200);
+        res.end(JSON.stringify(eliminado[0]));
+    }
+
+    // Ruta no encontrada
     else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Ruta no encontrada');
+        res.writeHead(404);
+        res.end(JSON.stringify({
+            mensaje: 'Ruta no encontrada'
+        }));
     }
 });
 
-servidor.listen(3000, '0.0.0.0', () => {
-    console.log('Servidor ejecutandose en el puerto 3000');
+servidor.listen(3000, 'localhost', () => {
+    console.log('Servidor local ejecutandose en http://localhost:3000');
 });
